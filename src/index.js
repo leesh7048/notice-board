57d96be716889e347e81be3ff5f5e4e3bbbda28b
seunghyun <tmdgus7048@naver.com>
2024-02-01 15:12:42 +0900

    router 세팅

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [{ index: true, element: <Home /> }],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
