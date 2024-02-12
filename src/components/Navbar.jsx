a67e9aee7c9a34351e5c0a73f50d21e44e686ea6
seunghyun <tmdgus7048@naver.com>
2024-02-12 16:05:11 +0900

    로고, 로그인 버튼 생성

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { PiNotepadBold } from "react-icons/pi";
+import { login, logout, onUserStateChange } from "../api/firebase";
 
 export default function Navbar() {
+  const [user, setUser] = useState();
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
+
+  useEffect(() => {
+    onUserStateChange((user) => setUser(user));
+  }, []);
   return (
-    <header className="flex justify-between border-b border-gray--300">
-      <Link className="text-3xl">
+    <header className="flex justify-between border-b border-gray--300 p-2">
+      <Link className="flex items-center text-3xl">
+        <PiNotepadBold />
         <h1>게시판</h1>
       </Link>
-      <button className="text-3xl">login</button>
+      {!user && (
+        <button className="text-3xl font-semibold" onClick={handleLogin}>
+          login
+        </button>
+      )}
+      {user && (
+        <button className="text-3xl font-semibold" onClick={handleLogout}>
+          logout
+        </button>
+      )}
     </header>
   );
 }
