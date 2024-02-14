f6e485d9a353f30d1e7f3a6f3db6b5d664aee47d
seunghyun <tmdgus7048@naver.com>
2024-02-14 17:42:41 +0900

    게시물 리스트 컴포넌트 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import List from "../components/List";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <List />
+      <Link to="/ItemInput">글쓰기</Link>
+    </>
+  );
 }
