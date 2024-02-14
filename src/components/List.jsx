75c75e34ae25aef755ebd8e21ba2ebb623045a3f
seunghyun <tmdgus7048@naver.com>
2024-02-14 18:11:22 +0900

    게시판 리스트 컴포넌트추가

---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./Post";
+
+export default function List() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Post />
+        </li>
+      </ul>
+    </div>
+  );
+}
