e878bf9d9cb1139620aa5a5a701302a8f4ed49e6
seunghyun <tmdgus7048@naver.com>
2024-02-14 16:57:32 +0900

    contextProvider로 컴포넌트 감싸기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <>
+    <AuthContextProvider>
       <Navbar />
       <Outlet />
-    </>
+    </AuthContextProvider>
   );
 }
 export default App;
