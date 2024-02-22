e8fcc6a6cb7514a3ee45c2d726c122741432a326
seunghyun <tmdgus7048@naver.com>
2024-02-22 18:30:51 +0900

    firebase database에 데이터 읽기,쓰기

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -6,7 +6,8 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, get } from "firebase/database";
+import { v4 as uuidv4 } from "uuid";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -31,5 +32,33 @@ export function logout() {
 export function onUserStateChange(callback) {
   onAuthStateChanged(auth, (user) => {
     callback(user);
+  });
+}
+
+export async function addPost(userInfo, post, date) {
+  const id = uuidv4();
+  set(ref(database, `/posts/${id}`), {
+    ...post,
+    uuid: id,
+    title: post.title,
+    content: post.content,
+    userId: userInfo.uid,
+    email: userInfo.email,
+    date: date,
+  });
+}
+
+// export async function getPost() {
+//   const gets = await get(ref(database, "user"));
+//   return gets.val();
+// }
+
+export async function getPost() {
+  const dbRef = ref(database, "posts");
+  return get(dbRef).then((snapshot) => {
+    if (snapshot.exists()) {
+      return Object.values(snapshot.val());
+    }
+    return [];
   });
 }
