d825fe5a2b052e3bf18a18685a007fe4db6d44ba
seunghyun <tmdgus7048@naver.com>
2024-02-22 18:29:55 +0900

    데이터베이스에서 post데이터 가져와서 출력, 시간별로 내림차순 정렬

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
+import { getPost } from "../api/firebase";
 
 export default function List() {
+  const [allPost, setAllPost] = useState();
+  useEffect(() => {
+    getPost().then((snap) => setAllPost(snap));
+  }, []);
+  allPost && console.log(sortDate(allPost));
+
+  function sortDate(list) {
+    const sorted_list = list
+      .sort(function (a, b) {
+        return new Date(a.date).getTime() - new Date(b.date).getTime();
+      })
+      .reverse();
+    return sorted_list;
+  }
+
   return (
     <div>
       <ul>
-        <li>
-          <Post />
-        </li>
+        {allPost?.map((post) => (
+          <Post key={post.uuid} post={post} />
+        ))}
       </ul>
     </div>
   );
