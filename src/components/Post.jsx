4e632d377966206529d6ef8ab65bfd14df62309d
seunghyun <tmdgus7048@naver.com>
2024-03-06 15:59:45 +0900

    게시판 게시물

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Post() {
-  return <div></div>;
+export default function Post({ post, index, allPostLength }) {
+  const { title, email, postId } = post;
+  const date = post.date.split("T")[0];
+  const postNumber = allPostLength - index;
+  const navigate = useNavigate();
+  return (
+    <tr
+      className=" border-collapse border-b-gray-300 border-b "
+      onClick={() => {
+        navigate(`/postDetail/${postId}`);
+      }}
+    >
+      <td className="text-center ">{postNumber}</td>
+      <td className="text-left pl-3 py-3">{title}</td>
+      <td className="text-center">{email}</td>
+      <td className="text-center">{date}</td>
+    </tr>
+  );
 }
