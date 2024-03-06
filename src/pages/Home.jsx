f72116a6c0ab96a1f3cd32c0582cb231a69b146b
seunghyun <tmdgus7048@naver.com>
2024-03-06 15:51:28 +0900

    글쓰기 버튼 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import List from "../components/List";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <>
       <List />
-      <Link to="/ItemInput">글쓰기</Link>
+      <button className="left-1" onClick={() => navigate("/postInput")}>
+        글쓰기
+      </button>
     </>
   );
 }
