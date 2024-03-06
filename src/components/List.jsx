730be73cf6d4d3630bb02290fb13cf88174dac4e
seunghyun <tmdgus7048@naver.com>
2024-03-06 15:58:56 +0900

    게시물 리스트로  저장 시간별로 나열

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,30 +1,41 @@
 import React, { useEffect, useState } from "react";
 import Post from "./Post";
-import { getPost } from "../api/firebase";
+import { getPosts } from "../api/firebase";
 
 export default function List() {
   const [allPost, setAllPost] = useState();
   useEffect(() => {
-    getPost().then((snap) => setAllPost(snap));
+    getPosts().then((snap) => setAllPost(sortDate(snap)));
   }, []);
-  allPost && console.log(sortDate(allPost));
 
   function sortDate(list) {
-    const sorted_list = list
-      .sort(function (a, b) {
-        return new Date(a.date).getTime() - new Date(b.date).getTime();
-      })
-      .reverse();
+    const sorted_list = [...list].sort(function (a, b) {
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    });
+
     return sorted_list;
   }
 
   return (
-    <div>
-      <ul>
-        {allPost?.map((post) => (
-          <Post key={post.uuid} post={post} />
+    <>
+      <table className="mt-6 w-full">
+        <thead>
+          <tr className="border-collapse border-b-gray-500 border-b">
+            <th className="w-10  py-3">번호</th>
+            <th className="w-100">제목</th>
+            <th>이메일</th>
+            <th>날짜</th>
+          </tr>
+        </thead>
+        {allPost?.map((post, index) => (
+          <Post
+            key={post.postId}
+            post={post}
+            index={index}
+            allPostLength={allPost.length}
+          />
         ))}
-      </ul>
-    </div>
+      </table>
+    </>
   );
 }
