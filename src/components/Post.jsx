7740c1c963cf804b21eca6ed6f0802a93e88d893
seunghyun <tmdgus7048@naver.com>
2024-03-18 18:07:55 +0900

    post number 수정

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function Post({ post, index, allPostLength }) {
+export default function Post({ post, allPost }) {
   const { title, email, postId } = post;
   const date = post.date.split("T")[0];
-  const postNumber = allPostLength - index;
+  const postNumber = allPost.length - allPost.indexOf(post);
   const navigate = useNavigate();
+
   return (
     <tr
       className=" border-collapse border-b-gray-300 border-b cursor-pointer"
@@ -14,7 +15,7 @@ export default function Post({ post, index, allPostLength }) {
       }}
     >
       <td className="text-center ">{postNumber}</td>
-      <td className="text-left pl-3 py-3">{title}</td>
+      <td className="text-left truncate  pl-3 py-3">{title}</td>
       <td className="text-center">{email}</td>
       <td className="text-center">{date}</td>
     </tr>
